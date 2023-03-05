3fcc6e6fa34d5c7d2767df017b2aa547c592f613
Danny <dannyjmargolin@gmail.com>
2023-03-05 13:41:21 -0600

    home page layout

--- pages/indexReal.js
+++ pages/indexReal.js
@@ -6,98 +6,113 @@ import VideoBar from "../src/components/VideoBar";
 import MaltaBar from "../src/components/MaltaBar";
 
 const Container = styled.div`
-  display: flex;
-  flex-direction: column;
-  /* padding-top: 30px; */
-  gap: 30px;
-  h1 {
-      font-size: 45px;
-      padding-top: 30px;
-      @media (max-width: 750px) {
-        text-align: center;
-      position: absolute;
-      top: 420px;
-      left: 0;
-      color: white;
-      font-size: 24px;
-      padding: 0 10px;
+    display: flex;
+    flex-direction: column;
+    /* padding-top: 30px; */
+    gap: 30px;
+    h1 {
+        font-size: 45px;
+        padding-top: 30px;
+        @media (max-width: 750px) {
+            text-align: center;
+            position: absolute;
+            top: 420px;
+            left: 0;
+            color: white;
+            font-size: 24px;
+            padding: 0 10px;
+        }
+        /* position: absolute; */
+        /* color: white; */
     }
-    /* position: absolute; */
-    /* color: white; */
-  }
-  img {
-    float: left;
-    max-width: 50vw;
-    height: auto;
-    padding-right: 15px;
-    padding-left: 15px;
+    img {
+        float: left;
+        max-width: 50vw;
+        height: auto;
+        padding-right: 15px;
+        padding-left: 15px;
 
-    @media (max-width: 750px) {
-      display: block;
-      float: none;
-      width: 100vw;
-      max-width: none;
-      margin: auto;
-      padding: 0;
-      padding-bottom: 14px;
+        @media (max-width: 750px) {
+            display: block;
+            float: none;
+            width: 100vw;
+            max-width: none;
+            margin: auto;
+            padding: 0;
+        }
     }
-  }
-  p {
-    text-align: left;
-    /* font-size: 25px; */
-    font-size: clamp(16px, 2vw, 35px);
-    /* font-size: 2.5vw; */
-    @media (max-width: 750px) {
-      font-size: 18px;
+    p {
+        text-align: left;
+        /* font-size: 25px; */
+        font-size: clamp(16px, 2vw, 35px);
+        /* font-size: 2.5vw; */
+        @media (max-width: 750px) {
+            font-size: 18px;
+        }
+        padding: 15px;
+        /* padding-left: 15px; */
     }
-    padding: 15px;
-    /* padding-left: 15px; */
-  }
 `;
 
 const Bio = styled.div`
-  display: flex;
-  @media (max-width: 750px) {
-    flex-direction: column;
-  }
+    display: flex;
+    @media (max-width: 750px) {
+        flex-direction: column;
+    }
 `;
 
 const Home = () => {
-  return (
-    <Container>
-      <Bio>
-        <Image
-          src={image}
-          height={500}
-          alt={"Portrait Photo od Dr. Zafra Lerman"}
-        />
-        <div>
-          <h1>
-            Zafra Lerman is a scientist, educator, and humanitarian recognized
-            for her contributions to science education and science diplomacy.
-          </h1>
-          <p>
-          She holds a PhD in chemistry from the Weizmann Institute of Science and conducted research on isotope effects at Cornell and Northwestern Universities in the US, and the ETH, Zurich, Switzerland. 
-She developed an innovative approach of teaching science at all levels using art, music, dance, drama, rap, and cultural backgrounds. These methods proved to be extremely successful among underprivileged students around the world and received international recognition. In 2000, she adopted the school district of the town of Soweto, South Africa (the birthplace of Mandela and Bishop Tutu). In the Chicago inner-city, she worked with students, teachers and parents and made a tremendous change in their attitudes towards science. She worked with homeless students at night in a dance studio and taught them science through dance (while bringing food too). Many of these students graduated from college and two continued for a PhD in biochemistry. She worked with prisoners who succeeded in receiving bachelor’s and master’s degrees. Over 16,000 Chicago public school students and more than 1,000 teachers participated in her programs. 
-            <br />
-            <br />
-            From 1986 to 2011, she chaired the American Chemical Society's Subcommittee on Scientific Freedom and Human Rights. She worked on human rights cases in the former Soviet Union, Russia, China, Guatemala, Cuba, Peru, South Africa, Iran, and many more. She met with dissidents in most of these countries. After meeting Andrei Sakharov in 1989 and taking his advice, she took a crash course in Russian to facilitate her work with dissidents in the former Soviet Union. At great risk to her safety, she succeeded in preventing executions, releasing prisoners of conscience from jail, and bringing dissidents to freedom. From 1987 until now, she serves as a Vice Chair for Chemistry on the Board of the Committee of Concerned Scientists (CCS), where she continues to be very active in human rights cases.  <br />
-            <br />
-            She is the recipient of many awards for education and science
-            diplomacy, including the 1999 Presidential Award from U.S. President
-            Clinton, the 2005 Nyholm Prize for Education from the Royal Society
-            of Chemistry (England), the 2015 Science Diplomacy Award from the
-            American Association for the Advancement of Science (AAAS), the 2016
-            Andrei Sakharov Award for human rights from the American Physical
-            Society (APS), and the 2016 United Nations NOVUS Award for the 16th
-            Sustainable Development Goal: Peace and Justice.
-          </p>
-        </div>
-      </Bio>
-      {/* <VideoBar />
+    return (
+        <Container>
+            <Bio>
+                <Image
+                    src={image}
+                    height={500}
+                    alt={"Portrait Photo od Dr. Zafra Lerman"}
+                />
+                <div>
+                    <h1>
+                        Zafra Lerman is a scientist, educator, and humanitarian
+                        recognized for her contributions to science education
+                        and science diplomacy.
+                    </h1>
+                    <p>
+                        She holds a PhD in chemistry from the Weizmann Institute
+                        of Science and conducted research on isotope effects at
+                        Cornell and Northwestern Universities in the US, and the
+                        ETH, Zurich, Switzerland. She developed an innovative
+                        approach of teaching science at all levels using art,
+                        music, dance, drama, rap, and cultural backgrounds.
+                        These methods proved to be extremely successful among
+                        underprivileged students around the world and received
+                        international recognition.
+                        <br />
+                        <br />
+                        From 1986 to 2011, she chaired the American Chemical
+                        Society's Subcommittee on Scientific Freedom and Human
+                        Rights. She worked on human rights cases in the former
+                        Soviet Union, Russia, China, Guatemala, Cuba, Peru,
+                        South Africa, Iran, and many more. She met with
+                        dissidents in most of these countries.
+                        <br />
+                        <br />
+                        She is the recipient of many awards for education and
+                        science diplomacy, including the 1999 Presidential Award
+                        from U.S. President Clinton, the 2005 Nyholm Prize for
+                        Education from the Royal Society of Chemistry (England),
+                        the 2015 Science Diplomacy Award from the American
+                        Association for the Advancement of Science (AAAS), the
+                        2016 Andrei Sakharov Award for human rights from the
+                        American Physical Society (APS), and the 2016 United
+                        Nations NOVUS Award for the 16th Sustainable Development
+                        Goal: Peace and Justice.
+                    </p>
+                </div>
+            </Bio>
+            {/* <VideoBar />
             <MaltaBar /> */}
-    </Container>
-  );
+        </Container>
+    );
 };
 
 export default Home;
--- src/components/Header.js
+++ src/components/Header.js
@@ -11,14 +11,14 @@ const headerMainHeight = "7em";
 const headerTopHeight = "2em";
 
 const HeaderContainer = styled.div`
-  color: white;
-  height: ${headerMainHeight + headerTopHeight};
-  h1,
-  p {
-    :hover {
-      color: lightgray;
+    color: white;
+    height: ${headerMainHeight + headerTopHeight};
+    h1,
+    p {
+        :hover {
+            color: lightgray;
+        }
     }
-  }
 `;
 // const HeaderTop = styled.div`
 //   height: ${headerTopHeight};
@@ -40,45 +40,46 @@ const HeaderContainer = styled.div`
 const mobileLogoTextWidth = "3.62em";
 
 const HeaderMain = styled.div`
-  /* margin-top: -1px; */
-  width: 100%;
-  height: ${headerMainHeight};
-  background-color: #3060A8;
+    /* margin-top: -1px; */
+    width: 100%;
+    height: ${headerMainHeight};
+    background-color: #3060a8;
 
-  /* @media screen and (min-width: 750px) {
+    /* @media screen and (min-width: 750px) {
     #siteTitle {
       text-align: center;
     }
 
   } */
 
-  > div {
-    height: 100%;
-    a {text-decoration: none;}
-    h1 {
-      margin-top: 0.13em;
-      color: white;
+    > div {
+        height: 100%;
+        a {
+            text-decoration: none;
+        }
+        h1 {
+            margin-top: 0.13em;
+            color: white;
+        }
+        display: flex;
+        flex-direction: column;
+        justify-content: space-around;
+        align-items: center;
     }
-    display: flex;
-    flex-direction: column;
-    justify-content: space-around;
-    align-items: center;
 
-  }
+    .headerIconButton {
+        background-color: #7b0000;
+        width: calc(${headerMainHeight} - 0.5em);
+        height: ${headerMainHeight};
 
-  .headerIconButton {
-    background-color: #7b0000;
-    width: calc(${headerMainHeight} - 0.5em);
-    height: ${headerMainHeight};
-
-    display: flex;
-    justify-content: center;
-    align-items: center;
-  }
-  .headerIconButton:active {
-    background-color: #660000;
-  }
-  /* #headerLogo {
+        display: flex;
+        justify-content: center;
+        align-items: center;
+    }
+    .headerIconButton:active {
+        background-color: #660000;
+    }
+    /* #headerLogo {
     width: calc(${headerMainHeight} + ${mobileLogoTextWidth});
     padding-left: 0.25em;
 
@@ -88,103 +89,100 @@ const HeaderMain = styled.div`
       margin: 0 0.4em 0 0.5em;
     }
   } */
-  .productType {
-    font-size: 1.4em;
-    padding: 0 0.2em;
-    /* margin: 0 0.8em; */
-  }
-
-  #headerMainCenter {
-    text-align: center;
-    width: 100%;
-    display: flex;
-    flex-flow: row wrap;
-    justify-content: space-around;
-
-    #siteTitle {
-      width: fit-content;
-      margin: 0.05em auto 0.15em;
-      font-size: 2.1em;
-      flex: 2 2 200%;
-      border-bottom: 1px solid white;
+    .productType {
+        font-size: 1.4em;
+        padding: 0 0.2em;
+        /* margin: 0 0.8em; */
     }
-  }
 
-  @media screen and (min-width: 800px) {
-    // should expand logo to be wider, maybe at smaller width?
-  }
+    #headerMainCenter {
+        text-align: center;
+        width: 100%;
+        display: flex;
+        flex-flow: row wrap;
+        justify-content: space-around;
+
+        #siteTitle {
+            width: fit-content;
+            margin: 0.05em auto 0.15em;
+            font-size: 2.1em;
+            flex: 2 2 200%;
+            border-bottom: 1px solid white;
+        }
+    }
+
+    @media screen and (min-width: 800px) {
+        // should expand logo to be wider, maybe at smaller width?
+    }
 `;
 
-
 const LinksContainer = styled.div`
-@media (max-width: 750px) {
-  /* display:none; */
-  display: none;
-}
+    @media (max-width: 750px) {
+        /* display:none; */
+        display: none;
+    }
 
-display: flex;
-width: 90%;
-max-width: 850px;
-justify-content: space-between;
+    display: flex;
+    width: 90%;
+    max-width: 850px;
+    justify-content: space-between;
 
-a {
-  color: white;
-  text-decoration:none;
-  font-size: 24px;
-}
-
+    a {
+        color: white;
+        text-decoration: none;
+        font-size: 24px;
+    }
 `;
 
 const BurgerMenu = styled.div`
-@media (min-width: 751px) {
-  display: none;
-}
-position: absolute;
-top: 36px;
-right: 36px;
+    @media (min-width: 751px) {
+        display: none;
+    }
+    position: absolute;
+    top: 36px;
+    right: 36px;
 `;
 
 const LogoContainer = styled.div`
-  position: absolute;
-  left: 50px;
-  justify-content: center;
-  align-items: center;
-  `;
-
+    position: absolute;
+    top: 20px;
+    left: 20px;
+    justify-content: center;
+    align-items: center;
+`;
 
 //COMPONENT STARTS HERE
 function Header() {
-
-
-
-  // if we want to hide search when user switch pages, maybe should add 'isSearching' to redux store
-  // also need to allow user to exit out by clicking elsewhere
-  return (
-    <HeaderContainer>
-      <HeaderMain>
-        <div>
-          <div id="headerMainCenter">
-            <LogoContainer>
-              <Image src={Logo} height={80}/>
-            </LogoContainer>
-            <Link href="/">
-              <h1 id="siteTitle">Zafra Lerman</h1>
-            </Link>
-            </div>
-            <BurgerMenu>
-              <NavBar />
-            </BurgerMenu>
-              <LinksContainer>
-                {links.map((link, idx) => {
-                  return (
-                    <Link id={link.id} key={idx} href={link.path}>{link.title}</Link>
-                    )
-                  })}
-              </LinksContainer>
-        </div>
-      </HeaderMain>
-    </HeaderContainer>
-  );
+    // if we want to hide search when user switch pages, maybe should add 'isSearching' to redux store
+    // also need to allow user to exit out by clicking elsewhere
+    return (
+        <HeaderContainer>
+            <HeaderMain>
+                <div>
+                    <div id="headerMainCenter">
+                        <LogoContainer>
+                            <Image src={Logo} height={80} />
+                        </LogoContainer>
+                        <Link href="/">
+                            <h1 id="siteTitle">Zafra Lerman</h1>
+                        </Link>
+                    </div>
+                    <BurgerMenu>
+                        <NavBar />
+                    </BurgerMenu>
+                    <LinksContainer>
+                        {links.map((link, idx) => {
+                            return (
+                                <Link id={link.id} key={idx} href={link.path}>
+                                    {link.title}
+                                </Link>
+                            );
+                        })}
+                    </LinksContainer>
+                </div>
+            </HeaderMain>
+        </HeaderContainer>
+    );
 }
 
 // disabling SSR for the header, because its contents depend on the localStorage
