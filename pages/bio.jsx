d72bc5d0dd96cd63b759490ebb7e911406bd50d2
Danny <dannyjmargolin@gmail.com>
2023-04-19 18:12:06 -0700

    pdf test

--- pages/bio.jsx
+++ pages/bio.jsx
@@ -10,17 +10,47 @@ const BioSection = styled.div`
   img {
     width: 50vw !important;
   }
+  .desktop-pdf {
+    display: none;
+  }
+
+  .mobile-pdf {
+    display: block;
+  }
+  @media only screen and (min-width: 750px) {
+    .mobile-pdf {
+      display: none;
+    }
+
+    .desktop-pdf {
+      display: block;
+    }
+  }
 `;
 
 const Bio = () => {
   return (
     <BioSection>
       <iframe
+        className="desktop-pdf"
         src="/ZafraLermanBio.pdf"
         type="application/pdf"
         width={"100%"}
         height={"100%"}
       ></iframe>
+      <iframe
+        className="mobile-pdf"
+        src="https://drive.google.com/viewerng/viewer?embedded=true&url=www.zafralerman.com/ZafraLermanBio.pdf"
+        width="100%"
+        height="90%"
+        type="application/pdf"
+        title="Title"
+      >
+        <a href="/ZafraLermanBio.pdf" target="_blank">
+          Please click here to attempt to view the information in a separate
+          browser window.
+        </a>{" "}
+      </iframe>
       <a href="/ZafraLermanBio.pdf" target="_blank" rel="noopener noreferrer">
         <button>Download Bio</button>
       </a>
